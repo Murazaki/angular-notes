5baf80f8de935662300cd9da1a64d265fb18ac2a
Mehdi Benadel <mehdi.benadel@gmail.com>
2017-01-25 10:42:13 +0100

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'iim-web-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: [ './app.component.css' ]
 })
 export class AppComponent {
-  title = 'app works!';
+  title;
+  count;
+
+  constructor() {
+    this.title = 'Hello World !';
+    this.count = 2;
+  }
+
+  getTitle() {
+    return this.title;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ElementComponent } from './element/element.component';
+import { TableauComponent } from './tableau/tableau.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ElementComponent,
+    TableauComponent
   ],
   imports: [
     BrowserModule,
